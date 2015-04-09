e020e287a7f03509df72a070059fe686ce14c4b8
Astro <astro@spaceboyz.net>
2015-04-09 17:30:34 +0200

    minimal site config for dresden

--- site.mk
+++ site.mk
@@ -2,20 +2,13 @@ GLUON_SITE_PACKAGES := \
 	gluon-mesh-batman-adv-14 \
 	gluon-alfred \
 	gluon-announced \
-	gluon-autoupdater \
-	gluon-config-mode-autoupdater \
 	gluon-config-mode-hostname \
-	gluon-config-mode-mesh-vpn \
 	gluon-config-mode-geo-location \
 	gluon-config-mode-contact-info \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
-	gluon-legacy \
 	gluon-luci-admin \
-	gluon-luci-autoupdater \
 	gluon-luci-portconfig \
-	gluon-next-node \
-	gluon-mesh-vpn-fastd \
 	gluon-radvd \
 	gluon-status-page \
 	bmxd \
